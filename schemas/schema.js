31dc31120a9b3781df4283fdbe3f58523ea08155
Kehinde Ojapa <k-ojapa@leadway.com>
2022-05-10 23:08:41 +0100

    added schemas for pin, comment, save and postedBy

--- schemas/schema.js
+++ schemas/schema.js
@@ -4,6 +4,10 @@ import createSchema from "part:@sanity/base/schema-creator";
 // Then import schema types from any plugins that might expose them
 import schemaTypes from "all:part:@sanity/base/schema-type";
 import user from "./user";
+import comment from "./comment";
+import pin from "./pin";
+import save from "./save";
+import postedBy from "./postedBy";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -14,5 +18,9 @@ export default createSchema({
   types: schemaTypes.concat([
     /* Your types here! */
     user,
+    comment,
+    pin,
+    save,
+    postedBy,
   ]),
 });
